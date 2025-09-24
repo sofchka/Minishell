8c127b639172b57079987baef742cc620ed37685
Marianna Tumanyan <mtumanya@c2r16s1.42yerevan.am>
2025-09-24 20:58:31 +0400

    builtin cd

--- src/builtins.c
+++ src/builtins.c
@@ -4,8 +4,8 @@ int	is_builtin(char *cmd)
 {
 	if (!cmd)
 		return (0);
-	return (/*!ft_strcmp(cmd, "echo") || !ft_strcmp(cmd, "cd")
-		|| */ !ft_strcmp(cmd, "pwd") /*|| !ft_strcmp(cmd, "export")
+	return (/*!ft_strcmp(cmd, "echo") || */!ft_strcmp(cmd, "cd")
+		|| !ft_strcmp(cmd, "pwd") /*|| !ft_strcmp(cmd, "export")
 		|| !ft_strcmp(cmd, "unset") || !ft_strcmp(cmd, "env")
 		|| !ft_strcmp(cmd, "exit")*/);
 }
@@ -14,13 +14,13 @@ int	exec_builtin(t_shell *sh, char **cmd)
 {
     (void)sh;
     // cd export unset exit aranc fork
-	if (!cmd )
+	if (!cmd)
 		return (1);
 	/*if (!ft_strcmp(cmd->cmd, "echo"))
-		return (ft_echo(&cmd->cmd));
-	else if (!ft_strcmp(cmd->cmd, "cd"))
-		return (ft_cd(sh, cmd));*/
-	if (!ft_strcmp(cmd[0], "pwd"))
+		return (ft_echo(&cmd->cmd));*/
+	else if (!ft_strcmp(cmd[0], "cd"))
+		return (ft_cd(sh, cmd));
+	else if (!ft_strcmp(cmd[0], "pwd"))
 		return (ft_pwd());
 	/*else if (!ft_strcmp(cmd->cmd, "export"))
 		return (ft_export(sh, &cmd->cmd));
@@ -47,5 +47,36 @@ int	ft_pwd(void)
 	free(pwd);
     return 0;
 }
+//cd
 
+int cmd_count(char **cmd)
+{
+	int i = 0;
+	while(cmd[i])
+		i++;
+	return i;
+}
+
+int ft_cd(t_shell *sh, char **cmd)
+{
+	int cd;
+	char *path;
+	//printf("lalalla");
+	//printf("%s",cmd[1]);
+	int ccmd = cmd_count(cmd);
+	if(ccmd > 2)
+		return (printf("bash: cd: too many arguments\n"),1);
+	int i = -1;
+	while(ccmd == 1 && sh->env[++i])
+	{
+		if((path = ft_strnstr(sh->env[i],"HOME=",5)) != NULL)
+		{
+			cd = chdir(path + 5);
+			return 0;
+		}
+		//printf("%s\n",sh->env[i]);
+	}
+	cd = chdir(cmd[1]);
+	return 0;
+}
 
--- src/minishell.h
+++ src/minishell.h
@@ -136,7 +136,7 @@ void	restore_std(t_shell *sh);
 char	*ft_join(char *a, char *b, char *c);
 
 // exit.c
-//int		ft_exit(char **argv, t_shell *shell);
+int		ft_exit(char **argv, t_shell *shell);
 
 // heredoc.c
 void	herdoc_handle(t_shell *sh, t_exec **data, int count);
@@ -145,12 +145,12 @@ char	*strip_quotes(const char *s);
 
 // builtins.c
 int	ft_echo(char **args);
-int	ft_cd(t_shell *sh, t_exec *cmd);
+int	ft_cd(t_shell *sh, char **cmd);
 int	ft_pwd(void);
 int	ft_export(t_shell *sh, char **args);
 int	ft_unset(t_shell *sh, char **args);
 int	ft_env(char **env);
-int	ft_exit(t_shell *sh, char **args);
+//int	ft_exit(t_shell *sh, char **args);
 int	is_builtin(char *cmd);
 int	exec_builtin(t_shell *sh, char **cmd);
 
